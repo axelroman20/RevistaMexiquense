20899515ae423c55aea50871cfed2d6eca19b0a7
Axel Roman <axelroman20@gmail.com>
2021-04-10 07:59:56 -0500

    error fixed  conexion den local

--- app/classes/database.php
+++ app/classes/database.php
@@ -13,10 +13,11 @@ class Database {
    * Constructor para nuestra clase
    */
   public function __construct() {
-    $this->engine  = IS_LOCAL ? LDB_ENGINE : DB_ENGINE;
-    $this->name    = IS_LOCAL ? LDB_NAME : DB_NAME;
-    $this->user    = IS_LOCAL ? LDB_USER : DB_USER;
-    $this->pass    = IS_LOCAL ? LDB_PASS : DB_PASS;
+    $this->engine  = IS_LOCAL ? LDB_ENGINE  : DB_ENGINE;
+    $this->host    = IS_LOCAL ? LDB_HOST    : DB_HOST;
+    $this->name    = IS_LOCAL ? LDB_NAME    : DB_NAME;
+    $this->user    = IS_LOCAL ? LDB_USER    : DB_USER;
+    $this->pass    = IS_LOCAL ? LDB_PASS    : DB_PASS;
     $this->charset = IS_LOCAL ? LDB_CHARSET : DB_CHARSET;
     return $this;    
   }
